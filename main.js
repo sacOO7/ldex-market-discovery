46d89d037ed67e663a7780bef897b0a8ebf0b21c
sacOO7 <sachinshinde7676@gmail.com>
2020-10-03 01:00:59 +0530

    Added passing test to return market with corresponding dex address

--- main.js
+++ main.js
@@ -4,24 +4,19 @@ import * as marketDiscovery from "./src";
 const leaseholdClient = ClientConfig().leaseHoldClient();
 
 
-export async function printDexWalletsUsingIterator() {
+export async function getDexMarketsUsingIterator() {
     let dexMarketPair = await marketDiscovery.getDexMarketPair(leaseholdClient);
     for await (let dexMarket of dexMarketPair) {
         console.log(dexMarket);
     }
 }
 
-export async function printDexWalletsUsingGenerator() {
+export async function getDexMarketsUsingGenerator() {
     let marketPairs = await marketDiscovery.getDexMarketPair(leaseholdClient);
     const pair = (await marketPairs.next()).value;
     console.log(pair);
 }
 
 (async () => {
-    await printDexWalletsUsingIterator();
-    // await printDexWalletsUsingGenerator();
+    await getDexMarketsUsingIterator();
 })()
-
-export const getLdexMarket  = () => {
-    return "lsk/eth";
-}
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,7 +1,12 @@
-import { getLdexMarket } from '../src/index';
-describe("Ldex Markets", () => {
-    it ("Should return proper ldex market", () => {
-        const market = getLdexMarket();
-        expect(market).toBe("lsk/eth");
+import {ClientConfig} from "../src/client-options";
+import * as marketDiscovery from "../src";
+describe("Ldex Markets",() => {
+    jest.setTimeout(60000);
+    it ("Should return right dex market for leasehold", async () => {
+        const leaseholdClient = ClientConfig().leaseHoldClient();
+        let marketPairs = await marketDiscovery.getDexMarketPair(leaseholdClient);
+        const pair = (await marketPairs.next()).value;
+        expect(pair.market).toBe("lsk");
+        expect(pair.dexWallet).toBe("7485409328757727573L");
     })
 });
